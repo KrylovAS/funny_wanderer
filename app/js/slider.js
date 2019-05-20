e3708230075ca3d7657acea43b484e0ad1858e8f
KrylovAS <s1nii@mail.ru>
2019-05-21 00:22:19 +0400

    h

---
+++ app/js/slider.js
@@ -0,0 +1,50 @@
+window.addEventListener('load', startSlider);
+
+function startSlider() {
+	function moveToSelected(element) {
+		const selected = document.getElementsByClassName('selected')[0];
+		const next = document.getElementsByClassName('next')[0];
+		const prev = document.getElementsByClassName('prev')[0];
+
+		switch (element) {
+			case 'next':
+				selected.classList.remove('selected');
+				selected.classList.add('prev');
+				next.classList.remove('next');
+				next.classList.add('selected');
+				prev.classList.remove('prev');
+				prev.classList.add('next');
+				break;
+			case 'prev':
+				selected.classList.remove('selected');
+				selected.classList.add('next');
+				next.classList.remove('next');
+				next.classList.add('prev');
+				prev.classList.remove('prev');
+				prev.classList.add('selected');
+				break;
+		}
+		setTimeout(hidden, 1000);
+
+		function hidden() {
+			document.getElementById('next').classList.remove('hiddenSlideBtn');
+			document.getElementById('prev').classList.remove('hiddenSlideBtn');
+		}
+	}
+
+	function hideBtn() {
+		document.getElementById('next').classList.add('hiddenSlideBtn');
+		document.getElementById('prev').classList.add('hiddenSlideBtn');
+	}
+
+	document.getElementById('prev').addEventListener('click', () => {
+		hideBtn();
+		moveToSelected('prev');
+	});
+
+	document.getElementById('next').addEventListener('click', () => {
+		hideBtn();
+
+		moveToSelected('next');
+	});
+}
